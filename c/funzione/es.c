685578329c7dafdf482203551af15d35460b085b
omgmomooo <5.z4rram72k@gmail.com>
2025-01-24 10:13:04 +0000

    senso

---
+++ c/funzione/es.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/*Scrivere una funzione chiamata "area_rettangolo" che prende in input la base e l'altezza di un rettangolo e restituisce l'area. 
+*/
+
+void area_rettangolo(int base, int altezza) {
+    int area = base * altezza;
+    printf("L'area del rettangolo è: %d\n", area);
+}
+
+int main() {
+    int base, altezza;
+    
+    
+    printf("Inserisci la base del rettangolo: ");
+    scanf("%d", &base);
+    
+    printf("Inserisci l'altezza del rettangolo: ");
+    scanf("%d", &altezza);
+    
+   
+    area_rettangolo(base, altezza);
+    
+    return 0;
+}
