d84a5402e3a08b1264bdafde5bfd2caac3706d1f
omgmomooo <5.z4rram72k@gmail.com>
2025-04-07 09:08:18 +0000

    update

--- battaglia_navale/step1.c
+++ battaglia_navale/step1.c
@@ -1,35 +1,27 @@
 #include <stdio.h>
 
-#define DIMENSIONE 5
+#define DIM 5
 
-void inizializzaCampo(char campo[DIMENSIONE][DIMENSIONE]);
-void visualizzaCampo(char campo[DIMENSIONE][DIMENSIONE]);
+void inizializza(char campo[DIM][DIM]) {
+    for (int i = 0; i < DIM; i++)
+        for (int j = 0; j < DIM; j++)
+            campo[i][j] = '~';
+}
 
-int main() {
-    char campo[DIMENSIONE][DIMENSIONE];
-    inizializzaCampo(campo);
-    visualizzaCampo(campo);
-    return 0;
-}
-void inizializzaCampo(char campo[DIMENSIONE][DIMENSIONE]) {
-    for (int riga = 0; riga < DIMENSIONE; riga++) {
-        for (int colonna = 0; colonna < DIMENSIONE; colonna++) {
-            campo[riga][colonna] = '~';
-        }
-    }
-}
-void visualizzaCampo(char campo[DIMENSIONE][DIMENSIONE]) {
-   printf("  ");
-    for (int col = 0; col < DIMENSIONE; col++) {
-       printf("%d ", col);
-    }
+void stampa(char campo[DIM][DIM]) {
+    for (int i = 0; i < DIM; i++) printf("%d ", i);
     printf("\n");
-
-    for (int riga = 0; riga < DIMENSIONE; riga++) {
-        printf("%d ", riga);
-        for (int colonna = 0; colonna < DIMENSIONE; colonna++) {
-            printf("%c ", campo[riga][colonna]);
-        }
+    for (int i = 0; i < DIM; i++) {
+        printf("%d ", i);
+        for (int j = 0; j < DIM; j++)
+            printf("%c ", campo[i][j]);
         printf("\n");
     }
+}
+
+int main() {
+    char campo[DIM][DIM];
+    inizializza(campo);
+    stampa(campo);
+    return 0;
 }
